579d18e6d28b7893cdc60a3178651faf5fd7fdbe
keshavkaartikeya <90041638+keshavkaartikeya@users.noreply.github.com>
2022-03-27 10:18:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload() {
+}
+function setup() {
+    canvas=createCanvas(360,360);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(360,360);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded() {
+    console.log('poseNet is initialized');
+}
+function gotPoses(results) {
+    if(results.lenght>0){
+        console.log(results);
+        console.log("nose x = "+ results[0].pose.nose.x);
+        console.log("nose y = "+ results[0].pose.nose.y);
+    }
+}
+function draw() {  
+    image (video,0,0,360,360);
+}
+
+function take_snapshot() {
+    save('myfilterImage.png');
+}
